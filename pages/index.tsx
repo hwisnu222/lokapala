57bc136150b76dfd4921fba8c3fbf4568997c736
Wisnu Box <hwisnu222@gmail.com>
2023-08-28 11:48:38 +0700

    feat: add bottom menu mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Archivo, Work_Sans } from "next/font/google";
-import { FaBars, FaXmark } from "react-icons/fa6";
+import { FaArrowRight, FaBars, FaXmark } from "react-icons/fa6";
 
 const archivo = Archivo({ subsets: ["latin"] });
 const work = Work_Sans({ subsets: ["latin"] });
@@ -31,6 +31,7 @@ const Container = (props: any) => {
 export default function Home() {
   const [drawer, setDrawer] = React.useState(false);
   const [sideMenu, setSideMenu] = React.useState(false);
+  const [bottomMenu, setBottomMenu] = React.useState(false);
 
   const handleDrawer = () => {
     setDrawer((prev: boolean) => !prev);
@@ -39,8 +40,43 @@ export default function Home() {
   const handleSideMenu = () => {
     setSideMenu((prev: boolean) => !prev);
   };
+
+  const handleBottomMenu = () => {
+    setBottomMenu((prev: boolean) => !prev);
+  };
   return (
     <>
+      {/* bottom menu mobile */}
+      <div className="bottom-menu mx-4 rounded-full z-30 fixed bottom-2 md:hidden flex border-b-2 border-orange-500 bg-gray-950">
+        <div
+          className="rounded-full bg-white p-4 flex justify-center items-center"
+          onClick={handleBottomMenu}
+        >
+          <FaArrowRight
+            className={`text-6xl text-orange-500 ${
+              bottomMenu ? "rotate-0" : "rotate-180"
+            }`}
+          />
+        </div>
+        <div
+          className={`bottom-menu flex items-center gap-2  overflow-x-hidden ${
+            bottomMenu ? " w-0" : "w-full"
+          }`}
+        >
+          <div className="flex justify-between gap-4 ml-2">
+            <div className="icon-wrap">
+              <img src="/icon-social/facebook.png" alt="" />
+            </div>
+            <div className="icon-wrap">
+              <img src="/icon-social/instagram.png" alt="" />
+            </div>
+            <div className="icon-wrap">
+              <img src="/icon-social/youtube.png" alt="" />
+            </div>
+          </div>
+          <img src="/google.png" alt="" className="w-auto mr-8" />
+        </div>
+      </div>
       {/* Side Menu */}
       <div
         className={`side-menu hidden md:flex fixed  items-center right-4 translate-y-[40%] z-[100] ${
@@ -249,143 +285,142 @@ export default function Home() {
             </div>
           </Container>
         </div>
-        <div className="bg-gray-950">
-          <div className="part-2 relative bg-left md:bg-center  md:object-cover width-full md:-mt-[75px]">
-            {/* <div className="overlay-right absolute top-0 w-full h-full"></div> */}
-            <Container className="grid md:grid-cols-2 gap-6  h-full">
-              <div></div>
-              <div className="flex justify-center pb-16 pt-44 flex-col items-start z-20">
-                <h3
-                  className={archivoFont(
-                    "uppercase font-extrabold text-7xl mb-6"
-                  )}
-                >
-                  what is lokapala?
-                </h3>
-                <p className={workSansFont("mb-8")}>
-                  Be the chosen Ksatriya and decide the fate of the realms!
-                  Experience the first MOBA in South East Asia: Lokapala!
-                  Developed by Indonesian game developer, Anantarupa Studios.
-                </p>
-                <button className="uppercase">what&apos;s more</button>
-              </div>
-            </Container>
-          </div>
 
-          <div className="part-3  relative mt-2 md:bg-center bg-right md:bg-no-repeat object-cover  w-full">
-            <div className="overlay-left absolute top-0 w-full h-full"></div>
-            <Container className="grid md:grid-cols-2 py-20 ">
-              <div className="flex justify-center flex-col items-start z-10">
-                <h3
-                  className={archivoFont(
-                    "uppercase font-extrabold text-7xl mb-6 "
-                  )}
-                >
-                  FIND OUT ABOUT KSATRIYA!
-                </h3>
-                <p className={workSansFont("mb-8")}>
-                  Be the chosen Ksatriya and decide the fate of the realms!
-                  Experience the first MOBA in South East Asia: Lokapala!
-                  Developed by Indonesian game developer, Anantarupa Studios.
-                </p>
-                <button className="uppercase">what&apos;s more</button>
-              </div>
-              <div></div>
-            </Container>
-          </div>
-
-          <div className="grid md:grid-cols-2 gap-2">
-            {Array.from({ length: 2 }).map((_, index) => (
-              <div
-                className="news hover:border-orange-500 border my-2"
-                key={index}
+        <div className="part-2 relative bg-left md:bg-center  md:object-cover width-full md:-mt-[70px]">
+          {/* <div className="overlay-right absolute top-0 w-full h-full"></div> */}
+          <Container className="grid md:grid-cols-2 gap-6  h-full">
+            <div></div>
+            <div className="flex justify-center pb-16 pt-44 flex-col items-start z-20">
+              <h3
+                className={archivoFont(
+                  "uppercase font-extrabold text-7xl mb-6"
+                )}
               >
-                <div className="overflow-hidden">
-                  <img
-                    src="/thumbnail/cat.png"
-                    alt=""
-                    className="origin-center"
-                  />
-                </div>
-                <div className="text-center p-6">
-                  <h3 className="uppercase font-extrabold text-3xl">
-                    Hot update
-                  </h3>
-                  <p className={workSansFont("opacity-75")}>
-                    Found out various info related to in-game Lokapala here!
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-
-          <footer className="border-t border-orange-500 pt-8 bg-cover">
-            <Container>
-              <div className="grid md:grid-cols-4 py-8 gap-16 md:gap4">
-                <div className="flex justify-center items-center w-full">
-                  <img src="/logo-footer.png" alt="" className="w-full" />
-                </div>
-                <div className="flex flex-col md:justify-start items-center">
-                  <h3 className="mb-6 font-extrabold text-2xl uppercase text-orange-500">
-                    menu
-                  </h3>
-                  <div>
-                    <ul>
-                      <li className="py-4 text-xl text-center md:text-left">
-                        Updates
-                      </li>
-                      <li className="py-4 text-xl text-center md:text-left">
-                        Event
-                      </li>
-                      <li className="py-4 text-xl text-center md:text-left">
-                        Ksatriya
-                      </li>
-                      <li className="py-4 text-xl text-center md:text-left">
-                        Support
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="flex flex-col items-center md:items-start">
-                  <h3 className=" mb-6 font-extrabold text-2xl uppercase text-orange-500">
-                    Install App
-                  </h3>
-                  <div className="flex flex-col gap-2 justify-start items-start">
-                    <div className="cursor-pointer ">
-                      <img src="/google.png" alt="" />
-                    </div>
-                    <div className="cursor-pointer ">
-                      <img src="/app-store.png" alt="" />
-                    </div>
-                  </div>
-                </div>
-                <div className="flex gap-2 justify-center md:justify-start">
-                  <div className="icon-wrap">
-                    <img src="/icon-social/facebook.png" alt="" />
-                  </div>
-                  <div className="icon-wrap">
-                    <img src="/icon-social/instagram.png" alt="" />
-                  </div>
-                  <div className="icon-wrap">
-                    <img src="/icon-social/discord.png" alt="" />
-                  </div>
-                  <div className="icon-wrap">
-                    <img src="/icon-social/youtube.png" alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="py-6 border-t border-orange-500 flex md:flex-row flex-col items-center gap-4 justify-between">
-                <span>
-                  Copyright © 2022 Anantarupa Studios. All Rights Reserved.
-                </span>
-                <ul className="flex gap-4">
-                  <li>Terms of Use</li>
-                  <li>Privacy Policy</li>
-                </ul>
-              </div>
-            </Container>
-          </footer>
+                what is lokapala?
+              </h3>
+              <p className={workSansFont("mb-8")}>
+                Be the chosen Ksatriya and decide the fate of the realms!
+                Experience the first MOBA in South East Asia: Lokapala!
+                Developed by Indonesian game developer, Anantarupa Studios.
+              </p>
+              <button className="uppercase">what&apos;s more</button>
+            </div>
+          </Container>
         </div>
+
+        <div className="part-3  relative mt-2 md:bg-center bg-right md:bg-no-repeat object-cover  w-full">
+          <div className="overlay-left absolute top-0 w-full h-full"></div>
+          <Container className="grid md:grid-cols-2 py-20 ">
+            <div className="flex justify-center flex-col items-start z-10">
+              <h3
+                className={archivoFont(
+                  "uppercase font-extrabold text-7xl mb-6 "
+                )}
+              >
+                FIND OUT ABOUT KSATRIYA!
+              </h3>
+              <p className={workSansFont("mb-8")}>
+                Be the chosen Ksatriya and decide the fate of the realms!
+                Experience the first MOBA in South East Asia: Lokapala!
+                Developed by Indonesian game developer, Anantarupa Studios.
+              </p>
+              <button className="uppercase">what&apos;s more</button>
+            </div>
+            <div></div>
+          </Container>
+        </div>
+
+        <div className="grid md:grid-cols-2 gap-2">
+          {Array.from({ length: 2 }).map((_, index) => (
+            <div
+              className="news hover:border-orange-500 border my-2"
+              key={index}
+            >
+              <div className="overflow-hidden">
+                <img
+                  src="/thumbnail/cat.png"
+                  alt=""
+                  className="origin-center"
+                />
+              </div>
+              <div className="text-center p-6">
+                <h3 className="uppercase font-extrabold text-3xl">
+                  Hot update
+                </h3>
+                <p className={workSansFont("opacity-75")}>
+                  Found out various info related to in-game Lokapala here!
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <footer className="border-t border-orange-500 pt-8 bg-cover">
+          <Container>
+            <div className="grid md:grid-cols-4 py-8 gap-16 md:gap4">
+              <div className="flex justify-center items-center w-full">
+                <img src="/logo-footer.png" alt="" className="w-full" />
+              </div>
+              <div className="flex flex-col md:justify-start items-center">
+                <h3 className="mb-6 font-extrabold text-2xl uppercase text-orange-500">
+                  menu
+                </h3>
+                <div>
+                  <ul>
+                    <li className="py-4 text-xl text-center md:text-left">
+                      Updates
+                    </li>
+                    <li className="py-4 text-xl text-center md:text-left">
+                      Event
+                    </li>
+                    <li className="py-4 text-xl text-center md:text-left">
+                      Ksatriya
+                    </li>
+                    <li className="py-4 text-xl text-center md:text-left">
+                      Support
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="flex flex-col items-center md:items-start">
+                <h3 className=" mb-6 font-extrabold text-2xl uppercase text-orange-500">
+                  Install App
+                </h3>
+                <div className="flex flex-col gap-2 justify-start items-start">
+                  <div className="cursor-pointer ">
+                    <img src="/google.png" alt="" />
+                  </div>
+                  <div className="cursor-pointer ">
+                    <img src="/app-store.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-2 justify-center md:justify-start">
+                <div className="icon-wrap">
+                  <img src="/icon-social/facebook.png" alt="" />
+                </div>
+                <div className="icon-wrap">
+                  <img src="/icon-social/instagram.png" alt="" />
+                </div>
+                <div className="icon-wrap">
+                  <img src="/icon-social/discord.png" alt="" />
+                </div>
+                <div className="icon-wrap">
+                  <img src="/icon-social/youtube.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="py-6 border-t border-orange-500 flex md:flex-row flex-col items-center gap-4 justify-between">
+              <span>
+                Copyright © 2022 Anantarupa Studios. All Rights Reserved.
+              </span>
+              <ul className="flex gap-4">
+                <li>Terms of Use</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </div>
+          </Container>
+        </footer>
       </main>
     </>
   );
